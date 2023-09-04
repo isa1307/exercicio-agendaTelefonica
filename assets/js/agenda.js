bddffbe81a8eb728c5a5b15b5dcc25906383c525
isakamitech <isabelasouzade.564@gmail.com>
2023-09-04 08:34:08 -0300

    feat: todas as verificaçoes de inputs corretas

--- assets/js/agenda.js
+++ assets/js/agenda.js
@@ -1,5 +1,5 @@
 class Usuario{
-    constructor(nome, telefone, celular, url, data, email, cep, cidade, instagram, github, id){
+    constructor(nome, telefone, celular, url, data, email, cep, cidade, instagram, github, signo, id){
         this.nome = nome;
         this.telefone = telefone;
         this.celular= celular;
@@ -10,6 +10,56 @@ class Usuario{
         this.cidade = cidade;
         this.instagram = instagram;
         this.github = github;
-        this.id = id; 
+        this.id = this.generateId; 
+        this.age = this.calculateAge();
+        this.signo = this.getZodiacSign();
     }
+    generateId() {
+        return Math.floor(Math.random() * 10000);
+    }
+}
+
+function adicionar(){
+    let nome = document.getElementById("input-nome").value;
+    let telefone = document.getElementById("input-telefone").value;
+    let celular = document.getElementById("input-celular").value;
+    let url = document.getElementById("input-url").value;
+    let data = document.getElementById("input-data").value;
+    let email = document.getElementById("input-email").value;
+    let cep = document.getElementById("input-cep").value;
+    let cidade = document.getElementById("input-cidade").value;
+    let insta = document.getElementById("input-insta").value;
+    let git = document.getElementById("input-git").value;
+
+
+    console.log(nome);
+    console.log(telefone);
+    console.log(celular);
+    console.log(url);
+    console.log(data);
+    console.log(email);
+    console.log(cep);
+    console.log(cidade);
+    console.log(insta);
+    console.log(git);
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function generateId() {
+    return Math.floor(Math.random() * 10000);
 }

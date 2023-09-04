fca6bfd7cfc46a6b11346d60436342f3840dd737
isakamitech <isabelasouzade.564@gmail.com>
2023-09-04 08:24:24 -0300

    feat: criacao da class usuario

---
+++ assets/js/agenda.js
@@ -0,0 +1,15 @@
+class Usuario{
+    constructor(nome, telefone, celular, url, data, email, cep, cidade, instagram, github, id){
+        this.nome = nome;
+        this.telefone = telefone;
+        this.celular= celular;
+        this.url = url;
+        this.data = data;
+        this.email = email;
+        this.cep = cep;
+        this.cidade = cidade;
+        this.instagram = instagram;
+        this.github = github;
+        this.id = id; 
+    }
+}
